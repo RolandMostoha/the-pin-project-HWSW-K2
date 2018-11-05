da71511806a8ff9b36bb991dff7e7868cfd991c6
Mostoha, Roland <mostoha.roland@autsoft.hu>
2018-11-05 12:57:01 +0100

    Naive implementation

--- app/src/main/java/hu/autsoft/demo/PinActivity.kt
+++ app/src/main/java/hu/autsoft/demo/PinActivity.kt
@@ -1,12 +1,10 @@
 package hu.autsoft.demo
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_pin.*
+import kotlinx.android.synthetic.main.content_pin.*
 
 class PinActivity : AppCompatActivity() {
 
@@ -15,25 +13,13 @@ class PinActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pin)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        submitPin.setOnClickListener {
+            if (pinInputField.text.toString() == "7878") {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                pinInputContainer.error = "Invalid PIN"
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_pin, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
